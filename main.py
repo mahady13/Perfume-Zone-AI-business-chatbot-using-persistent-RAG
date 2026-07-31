889756008f984d1e5c99f5a9984d443df4a8c6ec
mahady13 <mohiuddinmahady@gmail.com>
2026-07-31 08:53:25 +0600

    added the carousel for better customer engagement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import glob
 import os
+from streamlit_carousel import carousel
 from dotenv import load_dotenv
 from langchain_core.messages import AIMessage,HumanMessage
 from langchain_core.output_parsers import StrOutputParser
@@ -164,6 +165,39 @@ if "chat_history" not in st.session_state:
         AIMessage(content="Assalamu Alaikum vaiya/bon! Welcome to Perfume Zone. ✨ Ajke kon luxurious scent diye apnar mon bhalo korbo bolen? 🍊")
     ]
 
+perfume_banners = [
+    {
+        "title": "",
+        "text": "",
+        "img": "https://scontent.fdac198-2.fna.fbcdn.net/v/t39.30808-6/753255019_1019734910871492_8371982263359702305_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGH_IVCS_llaqhPkTE_ZjOz-Y3sWK16vpH5jexYrXq-kVtMkDkxXS23IUXD9XgdvwRd4XkUBNyisdgP14bWEJRR&_nc_ohc=l0rhr8H2XmQQ7kNvwFGp5Yu&_nc_oc=AdoAmBp55fzOjo36cIHT_ULWViD3zFlfYFF518PfrE0i22rkq83LV3SbS-Mm_9R_D8ywwHB5GH-a9K0YhBhLVXlv&_nc_zt=23&_nc_ht=scontent.fdac198-2.fna&_nc_gid=jMVaXYQL6UxZkjB-SDAhFQ&_nc_ss=7b2a8&oh=00_AQFnb0UMoz7wK3vLtSjLvZ1E-KozsyDKRfAythRSHOi-7w&oe=6A71CBA3", # এখানে আপনার আসল ইমেজ লিংক বসাবেন
+    },
+    {
+        "title": "",
+        "text": "",
+        "img": "https://scontent.fdac24-1.fna.fbcdn.net/v/t39.30808-6/753306595_1018971857614464_140812124907187197_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGTybUudH3iJcELmglq5KmSZxfyVHwd8fRnF_JUfB3x9N0pRC0MSusaXmpT4vB4uz_5U6jfrnYpU5zN_XjIO2-7&_nc_ohc=ukax4ALIp-IQ7kNvwFNKgTW&_nc_oc=Adrtclh6RfqG9VqeCrebBblgZxznHY3AtFXlYCIZ1pOwzcb0MHny_ZYqiQOhnTLVSbag9qUaPA8ZhgknzpCYQGOp&_nc_zt=23&_nc_ht=scontent.fdac24-1.fna&_nc_gid=EZxuqe1UPB_S6TI2JNTpTQ&_nc_ss=7b2a8&oh=00_AQGBOJM9ybcRGSpuLKARVSN2amPKz_43BNNdo13GgQjdlw&oe=6A71E317",
+    },
+    {
+        "title": "",
+        "text": "",
+        "img": "https://scontent.fdac24-5.fna.fbcdn.net/v/t39.30808-6/746026711_1012032108308439_306530557166542663_n.jpg?stp=dst-jpg_tt6&cstp=mx1254x1254&ctp=s1254x1254&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeElUKi9Iy_becyQ7xwYcrP6wF6mHxeg4JLAXqYfF6Dgktzp0R9eWbeXXJxe1xnWNKweLruSJQ2efQ7dhM6drf_7&_nc_ohc=IJAn4rumjaMQ7kNvwEYqDvu&_nc_oc=Adp4F4CLY0_5JcFuuh62v_wPfSrgLOTSSswAgPUSmb-maeQr2aG99lbdkQ3CIZ0aH_URMsbGdujLCDx9N6DqU0bh&_nc_zt=23&_nc_ht=scontent.fdac24-5.fna&_nc_gid=h-1BCjN6j3K9j65bKwSAlg&_nc_ss=7b2a8&oh=00_AQF_GDOmcViTGRhpgrIcanGFu5KKBjINM4zhuMCE-6Ymzg&oe=6A71DBDA", # এখানে আপনার আসল ইমেজ লিংক বসাবেন
+    },
+    {
+        "title": "",
+        "text": "",
+        "img": "https://scontent.fdac24-5.fna.fbcdn.net/v/t39.30808-6/635454359_889551030556548_8145614045911123595_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFlWvooZ6_eBS0Pm8opxFbf04D3O_q7bMPTgPc7-rtsw_TiIUPjdQdJruouDbWScsIm2RAKH4dt9exI4qDbQIFi&_nc_ohc=_aJL_Bw4WQEQ7kNvwFFo9J9&_nc_oc=Adosi8nldCC2fnC3n3EZxKjLRVfc9jDAQWyEiHYaVrp-_jcOeZESGRnc_XKiDt2nmVcCmzEQrZw4Ft-nVsHzSU3b&_nc_zt=23&_nc_ht=scontent.fdac24-5.fna&_nc_gid=exrOejJF5E3YX6h_rlcOYQ&_nc_ss=7b2a8&oh=00_AQG3BHAhyeFkk_9TGR-E6B-jKIiVhkZTD0-CiIOM-MsP_w&oe=6A71CC8F", # এখানে আপনার আসল ইমেজ লিংক বসাবেন
+    },
+    {
+        "title": "",
+        "text": "",
+        "img": "https://scontent.fdac198-2.fna.fbcdn.net/v/t39.30808-6/738691597_1004330489078601_7107487147553559495_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFs0VIv8f2UuvHxcRRiQFGGDw5TOknRCI4PDlM6SdEIjgZao1U1jRTv_YSxO_8aGDVr0DWBPtERRynaz9dnAcL0&_nc_ohc=ltkkSsHeAbUQ7kNvwH0LDI7&_nc_oc=AdoaIuiXljn9k-s-bSA0RqE4_04-js0pm-QRvI21l0aUwapscdM9mBn0htXgaeHDL0xhsXn8RuwD52GZqqD83nKk&_nc_zt=23&_nc_ht=scontent.fdac198-2.fna&_nc_gid=dAhSp7_ppd22XwOm7UtYsA&_nc_ss=7b2a8&oh=00_AQGWTPnBEy6IX1Qn0kw9qn4ZormZNFns9TeIJqOi7fstxw&oe=6A71E2AD", # এখানে আপনার আসল ইমেজ লিংক বসাবেন
+    }
+]
+
+st.write("---")
+carousel(items=perfume_banners, width=1.0)
+st.write("---")
+
+st.subheader("💬 Chat with Perfume Zone AI")
 for message in st.session_state.chat_history:
     if isinstance(message,AIMessage):
         with st.chat_message("assistant"):
