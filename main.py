e487e23200d740a7286616c681eeae1a4a25ad4a
mahady13 <mohiuddinmahady@gmail.com>
2026-08-01 13:58:58 +0600

    doneeeeee yeeeeeessss,solved the langchain retriver problem

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from langchain_groq import ChatGroq
 from langchain_chroma import Chroma
 # from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.retrievers import BM25Retriever
-from langchain.retrievers import EnsembleRetriever
+from langchain_classic.retrievers import EnsembleRetriever
 
 
 
